9227802a0a60ccc5ba18c7c41eda019794bf16f6
jasonschreib <jwschreiber1@gmail.com>
2020-11-30 16:32:25 -0800

    Nested components

--- app.js
+++ app.js
@@ -1,6 +1,33 @@
 // TODO
+//create components for each of the grocerylist items
+var Beans = () => (
+  <div>Beans</div>
+);
+
+var Potatoes = () => (
+  <div>Potatoes</div>
+);
+
+var Eggs = () => (
+  <div>Eggs</div>
+);
+
+
+// var GroceryList = () => (
+//   <div>Beans
+//     <div>Potatoes
+//       <div>Eggs</div>
+//     </div>
+//   </div>
+// );
+
 var GroceryList = () => (
-  <div>Beans, Potatoes</div>
+  <div>
+    <h2>My Grocery List</h2>
+    <Beans />
+    <Potatoes />
+    <Eggs />
+  </div>
 );
 
 ReactDOM.render(<GroceryList />, document.getElementById("app"));
